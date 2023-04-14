94a8e50b8c8666136983f35a12d94ca46556742d
gluke0 <milanesigianluca@gmail.com>
2023-04-14 15:57:41 +0200

    es 3 array

--- es-3/assets/js/main.js
+++ es-3/assets/js/main.js
@@ -4,3 +4,20 @@ l'array generato da parte vostra dovrà essere di ad esempio nomi o cose: ['albe
 se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter. */
+
+// numero random tra max e min
+let rNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const techBrands = [
+    'Apple',
+    'Google',
+    'Microsoft',
+    'Bose',
+    'Leica',
+    'Meta',
+    'Oracle'
+]
+
+console.log(techBrands)
