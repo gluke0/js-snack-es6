4d07972a3cde884a4889a0e108445a29059484af
gluke0 <milanesigianluca@gmail.com>
2023-04-14 17:15:28 +0200

    esercizio 2 da sistemare

--- es-2/assets/js/main.js
+++ es-2/assets/js/main.js
@@ -4,3 +4,46 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+const teams =  [
+    {
+        team: 'Inter',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        team: 'Atalanta',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        team: 'Milan',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        team: 'Monza',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        team: 'Cremonese',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        team: 'Verona',
+        points: 0,
+        fouls: 0,
+    }
+];
+
+teams.forEach( ( data ) => {
+    data.points = Math.floor(Math.random() * 100) + 1;
+    data.fouls = Math.floor(Math.random() * 100) + 1;
+});
+
+const teamFouls = teams.map( ( {team, fouls} ) => 
+     { return team + fouls});
+     
+console.log(teamFouls)
